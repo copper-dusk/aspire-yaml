d837cd7e1acc8675a88334f0277b2c3459dfaaae
Alexander Trauzzi <acj@trauzzi.me>
2026-05-22 18:59:23 -0500

    Small bug fix to ensure dir already exists.

--- Hosting.Yaml/TempYamlPath.cs
+++ Hosting.Yaml/TempYamlPath.cs
@@ -7,6 +7,9 @@ public static class TempYamlPath
     public static string BuildTempYamlPath(string suffix)
     {
         var tempRoot = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        var tempWorkingDirecotry = Path.Join(tempRoot, WorkingDirectory);
+        
+        Directory.CreateDirectory(tempWorkingDirecotry);
 
         return Path.Join(tempRoot, WorkingDirectory, suffix);
     }
